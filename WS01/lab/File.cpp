0a6ce3ede91f653b329bfeaed65a821a6f2b93fb
NishitShah18 <ngshah3@myseneca.ca>
2022-05-20 20:38:20 -0400

    added date in comment

--- WS01/lab/File.cpp
+++ WS01/lab/File.cpp
@@ -1,12 +1,14 @@
 /*************************************************************************
 
 Name			: Nishit Gaurang Shah
-Student number	: 130 176 217
+Student number		: 130 176 217
 Email Id		: ngshah3@myseneca.ca
 Section			: OOP244NFF
 
+Date : 20th May 2022
+
 Autheticity Declaraition :
-I declare that I have done all the coding by myself and only copied the 
+I declare that I have done all the coding by myself and only copied the
 code that my professor provided to complete my workshops and assignments.
 
 *************************************************************************/
--- WS01/lab/File.h
+++ WS01/lab/File.h
@@ -1,16 +1,17 @@
 /*************************************************************************
 
 Name			: Nishit Gaurang Shah
-Student number	: 130 176 217
+Student number		: 130 176 217
 Email Id		: ngshah3@myseneca.ca
 Section			: OOP244NFF
+
+Date : 20th May 2022
 
 Autheticity Declaraition :
 I declare that I have done all the coding by myself and only copied the
 code that my professor provided to complete my workshops and assignments.
 
 *************************************************************************/
-
 // SAFE-GAURD
 #ifndef SDDS_FILE_H
 #define SDDS_FILE_H
