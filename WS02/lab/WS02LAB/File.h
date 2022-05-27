8f8373f85835fcf430e869c21f79fbb735049800
NishitShah18 <ngshah3@myseneca.ca>
2022-05-27 18:37:34 -0400

    Completed LAB-2

---
+++ WS02/lab/WS02LAB/File.h
@@ -0,0 +1,34 @@
+/*************************************************************************
+Workshop 2 Part 1
+Full Name   : Nishit Gaurang Shah
+Student ID# : 130 176 217
+Email       : ngshah3@myseneca.ca
+Section     : OOP244NFF
+
+Date : 27th May 2022
+
+Autheticity Declaraition :
+I declare that I have done all the coding by myself and only copied the
+code that my professor provided to complete my workshops and assignments.
+
+*************************************************************************/
+
+// SAFE-GAURD
+#ifndef SDDS_FILE_H_
+#define SDDS_FILE_H_
+
+namespace sdds {
+
+	//////////////////////////
+	// FUNCTION PROTOTYPES 
+	//////////////////////////
+	bool openFile(const char filename[]);
+	void closeFile();
+
+	int noOfRecords();
+	
+	bool read(char* name);
+	bool read(int& studentNumber);
+	bool read(char& grade);
+}
+#endif // !SDDS_FILE_H_
