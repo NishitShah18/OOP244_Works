82002af00a3524b6dbcc074d9bf98a4569870856
Nishit Shah <ngshah3@myseneca.ca>
2022-09-12 21:43:59 -0400

    Path edited

--- WS04/lab/main.cpp
+++ WS04/lab/main.cpp
@@ -1,3 +1,18 @@
+/*************************************************************************
+Workshop 4 Part 1
+Full Name   : Nishit Gaurang Shah
+Student ID# : 130 176 217
+Email       : ngshah3@myseneca.ca
+Section     : OOP244NFF
+
+Date : 10th June 2022
+
+Autheticity Declaraition :
+I declare that I have done all the coding by myself and only copied the
+code that my professor provided to complete my workshops and assignments.
+
+*************************************************************************/
+
 /* ------------------------------------------------------
 Workshop 4 part 1
 Filename: main.cpp
