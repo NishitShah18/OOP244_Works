79c2fddb14925323ee4c172f14e6c129fb92f925
Nishit Shah <ngshah3@myseneca.ca>
2022-09-19 16:44:31 -0400

    revision done, changed paths

--- WS02/lab/gradeReport.cpp
+++ WS02/lab/gradeReport.cpp
@@ -9,6 +9,22 @@ Revision History
 -----------------------------------------------------------
 Date       Reason
 -----------------------------------------------------------*/
+
+/*************************************************************************
+Workshop 2 Part 1
+Full Name   : Nishit Gaurang Shah
+Student ID# : 130 176 217
+Email       : ngshah3@myseneca.ca
+Section     : OOP244NFF
+
+Date : 27th May 2022
+
+Autheticity Declaraition :
+I declare that I have done all the coding by myself and only copied the
+code that my professor provided to complete my workshops and assignments.
+
+*************************************************************************/
+
 #include "Student.h"
 using namespace sdds;
 int main() {
